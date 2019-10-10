54aeeb4f60a76290e7c869f14fd8bc8b21582cc9
Artiium Arbyummi <artiium21@gmail.com>
2019-10-10 02:59:13 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,8 +1,8 @@
-catch_phrase = "Hooray! Mario has rescued Princess Peach"
+
 
 def rescue_princess_peach(catch_phrase)
   status = "rescued"
-  puts catch_phrase
+  puts "Hooray! Mario has rescued Princess Peach"
 end
 
 ## Comment the below back in to see method scope in action!
